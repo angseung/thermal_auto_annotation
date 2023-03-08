d50193619dba78ae1b94580c81bb1d416f294f87
angseung <swim@surromind.ai>
2023-03-09 08:51:18 +0900

    Added target dir mkdir routine

--- split_video_into_frame.py
+++ split_video_into_frame.py
@@ -1,8 +1,15 @@
+import os
 import cv2
 
+# video_file_name = "standing_a_person"
 video_file_name = "standing_people"
+# video_file_name = "lie"
 capture = cv2.VideoCapture(f"videos/{video_file_name}.mkv")
 frameNr = 0
+target_dir = f"images/{video_file_name}"
+
+if not os.path.isdir(target_dir):
+    os.makedirs(target_dir, exist_ok=True)
 
 xtl, ytl = 482, 257
 w, h = 262, 202
@@ -12,7 +19,7 @@ while True:
     if success:
         if frameNr % 10 == 0:
             frame = frame[ytl : ytl + h, xtl : xtl + w, :]
-            cv2.imwrite(f"images/{video_file_name}/frame_%05d.jpg" % frameNr, frame)
+            cv2.imwrite(f"{target_dir}/frame_%05d.jpg" % frameNr, frame)
 
     else:
         break
